f7ccebbfd962b1b9baa6abad384ea384d25bb1eb
AlinaMertina <mertinaclaudietoto@gmail.com>
2025-02-26 21:39:29 +0300

    midlleware

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const helper=require('./helper')
+const {success}=require('./helper')
 let pokemons = require('./db/mock-pokemons')
 const app = express();
 const port = 3000;
@@ -9,16 +9,28 @@ app.get('/', (req, res) => {
     res.send("Hello Express! xd");
 });
 
+
+app.use((req, res,next) => {
+    console.log("Hello Express! xd");
+    next();
+});
+app.use((req, res,next) => {
+    console.log("Hello Express 1! xd");
+    next();
+});
+
+
 app.get('/api/pokemons/:id', (req, res) => {
     const id=req.params.id;
     const pokemon=pokemons.find(pokemon => pokemon.id ==id)
     console.log(pokemon)
     const message="UN pokemon a bien ete trouver";
-    res.json(helper.success(message,pokemon));
+    res.json(success(message,pokemon));
 });
 
 app.get('/api/pokemons', (req, res) => {
-    res.send(`IL y a aux total ${pokemons.length} pokeman`);
+    const message ="la liste de pokemons";
+    res.json(success(message,pokemons));
 });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
